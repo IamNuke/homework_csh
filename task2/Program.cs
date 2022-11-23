35a001219bd6ba4d331b755896653c3d1fc270fc
IamNuke <alexeimartynyuk@gmail.com>
2022-11-23 20:19:35 +0300

    Task 2

---
+++ task2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.Clear();
+
+Console.Write("Enter first number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter third number: ");
+int k = Convert.ToInt32(Console.ReadLine());
+
+int max = n;
+if(m > max)
+  max = m;  
+if(k > max)
+    max = k;
+
+
+Console.Write($"max = {max}");    
