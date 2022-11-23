d90702d9c9895ea571be9e5dc15db15eb4b549e0
IamNuke <alexeimartynyuk@gmail.com>
2022-11-23 19:57:51 +0300

    First task

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Clear();
+
+Console.Write("Enter first number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+
+if(n > m)
+{
+    Console.Write($"max = {n}");    
+}
+else
+{
+    Console.Write($"max = {m}");    
+}
+
