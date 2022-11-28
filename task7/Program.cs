49199763cae9a966d24d78b008eacafdebf6072e
IamNuke <alexeimartynyuk@gmail.com>
2022-11-28 17:00:45 +0300

    Task 7

--- task7/Program.cs
+++ task7/Program.cs
@@ -1,1 +1,17 @@
 ﻿
+Console.Clear();
+
+Console.Write("Enter number: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > 0 && n < 8){
+  if (n == 6 || n == 7) {
+    Console.Write("yes");
+  }
+  else{
+    Console.Write("no");
+  }  
+}
+else{
+    Console.Write("wrong parameter!");
+}
