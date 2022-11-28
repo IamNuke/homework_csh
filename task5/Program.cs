2bece6821b488103a414ed4ba1bf7bdba5096f6e
IamNuke <alexeimartynyuk@gmail.com>
2022-11-28 16:29:33 +0300

    Task 5

--- task5/Program.cs
+++ task5/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Clear();
+
+Console.Write("Enter number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n / 100 > 0 && n / 100 <= 9)
+    Console.Write((n/10)%10);
+else
+    Console.Write("wrong parameter!");
