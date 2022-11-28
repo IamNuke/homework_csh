b9bffc4c381c9bdd519683ee61d2ba32b180bbc6
IamNuke <alexeimartynyuk@gmail.com>
2022-11-28 16:50:31 +0300

    Task 6

--- task6/Program.cs
+++ task6/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Clear();
+
+Console.Write("Enter number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = n;
+
+if (n/100 == 0){
+    Console.Write("no third digit!");
+}
+else{
+    if (m<0)
+        m = -m;
+        
+    while (m > 1000){
+        m = m/10;
+    }    
+    Console.Write(m%10);
+}
