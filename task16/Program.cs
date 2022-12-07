e377715282d1a3194f1279f9a20a56d583eef8dc
IamNuke <alexeimartynyuk@gmail.com>
2022-12-07 20:26:34 +0300

    Task 16

---
+++ task16/Program.cs
@@ -0,0 +1,23 @@
+﻿void InputArray(int[] array, int a, int b)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(a, b); 
+}
+
+
+int NumOfEvenNumbers(int[] array)
+{     
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)           
+       if (array[i]%2 == 0)
+        count++;
+    return count;        
+}
+
+//Console.Clear();
+Console.Write("Enter number of elements: ");
+int n =Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array, 100, 1000);
+Console.WriteLine($"input :[{string.Join(", ", array)}]");
+Console.WriteLine($"output :{NumOfEvenNumbers(array)}");
